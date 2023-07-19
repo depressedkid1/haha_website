1647820195c5b43f16e569e5fbaef064fa993335
depressedkid1 <urgant1337@list.ru>
2023-07-19 18:25:36 +0900

    Modified template

--- hihi/models.py
+++ hihi/models.py
@@ -6,6 +6,7 @@ from django.utils import timezone
 class People(models.Model):
     title = models.CharField(max_length=255)
     img = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    vk_id = models.CharField(max_length=255,default='404')
     content = models.TextField(blank=True)
     time_create = models.DateTimeField(default=timezone.now)
     time_publish = models.DateTimeField(blank=True, null=True)
--- hihi/views.py
+++ hihi/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import People
+from django.utils import timezone
 
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'hihi/post_list.html', {})
+    posts = People.objects.filter(time_publish__lte=timezone.now()).order_by('time_publish')
+    return render(request, 'hihi/post_list.html', {'posts': posts})
