67de9eb9134f00f2bfc52886038e325d9747e6ec
depressedkid1 <urgant1337@list.ru>
2023-07-19 13:57:20 +0900

    HIHIHAHA WEBSITE

---
+++ hihi/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+
+
+class People(models.Model):
+    title = models.CharField(max_length=255)
+    img = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    content = models.TextField(blank=True)
+    time_create = models.DateTimeField(default=timezone.now)
+    time_publish = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.time_publish = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
